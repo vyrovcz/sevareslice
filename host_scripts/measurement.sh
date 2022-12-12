b3a83ce2a2996da022da3200ad16c6685afb1917
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2022-12-12 21:07:06 +0100

    test possible problem

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -103,7 +103,7 @@ pos_sync --timeout 300
 if [ "$splitroles" == 0 ]; then
     /bin/time -f "$timerf" ./search-P"$player".o "$ipA" "$ipB" &>> testresults || success=false
 else
-    /bin/time -f "$timerf" ./Scripts/split-roles.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
+    ./Scripts/split-roles.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
 fi
 
 pos_sync
