d6ee8fe65019b0d6937ed5edc49b832c099af271
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2022-12-13 00:05:15 +0100

    deactive external runtime measurement for split ro

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -105,10 +105,10 @@ if [ "$splitroles" == 0 ]; then
 else
     #doesn't work with /bin/time
     #/bin/time -f "$timerf" ./Scripts/split-roles.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
-    #./Scripts/split-roles.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
+    ./Scripts/split-roles.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
 
-    echo "./Scripts/split-roles.sh -p \"$1\" -a \"$2\" -b \"$3\"" > run.sh
-    /bin/time -f "$timerf" bash run.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
+    ###echo "./Scripts/split-roles.sh -p \"$1\" -a \"$2\" -b \"$3\"" > run.sh
+    ###/bin/time -f "$timerf" bash run.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
     
     # calculate mean of 6 numbers
     sum=$(grep "computation clock" testresults | cut -d 's' -f 2 | awk '{print $6}' | paste -s -d+ | bc)
