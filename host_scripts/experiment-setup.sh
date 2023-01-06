fc3e267cc85cab3aa22e3ae8d5879d93525f41c5
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2023-01-06 22:11:39 +0100

    use new 25G link

--- host_scripts/experiment-setup.sh
+++ host_scripts/experiment-setup.sh
@@ -37,28 +37,31 @@ ips=()
 ### three nodes indirect connection topology setup
 ### node 2 --- node 1 --- node 3
 ### for 25G+ speeds set MTU
-if [ "$(hostname | grep -cE "gard|goracle|zone")" -eq 1 ]; then
-
-	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic0"
-	ip link set dev "$nic0" mtu 9700
-	ip link set dev "$nic0" up
-
-	if [ "$ipaddr" -eq 2 ]; then
-		# activate forwarding for the center node
-		sysctl -w net.ipv4.ip_forward=1
-		ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic1"
-		ip link set dev "$nic1" mtu 9700
-		ip link set dev "$nic1" up
-		# route via correct NICs
-		ip route add 10.10."$network".3 dev "$nic0"
-		ip route add 10.10."$network".4 dev "$nic1"
-	elif [ "$ipaddr" -eq 3 ]; then
-		ip route add 10.10."$network".4 via 10.10."$network".2
-	else
-		ip route add 10.10."$network".3 via 10.10."$network".2
-	fi
+####if [ "$(hostname | grep -cE "gard|goracle|zone")" -eq 1 ]; then
+####
+####	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic0"
+####	ip link set dev "$nic0" mtu 9700
+####	ip link set dev "$nic0" up
+####
+####	if [ "$ipaddr" -eq 2 ]; then
+####		# activate forwarding for the center node
+####		sysctl -w net.ipv4.ip_forward=1
+####		ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic1"
+####		ip link set dev "$nic1" mtu 9700
+####		ip link set dev "$nic1" up
+####		# route via correct NICs
+####		ip route add 10.10."$network".3 dev "$nic0"
+####		ip route add 10.10."$network".4 dev "$nic1"
+####	# this is probably not required since the routes should be set automatically
+####	elif [ "$ipaddr" -eq 3 ]; then
+####		ip route add 10.10."$network".4 via 10.10."$network".2
+####	else
+####		ip route add 10.10."$network".3 via 10.10."$network".2
+####	fi
+##### three nodes direct connection topology if true
+####elif [ "$nic1" != 0 ]; then
 # three nodes direct connection topology if true
-elif [ "$nic1" != 0 ]; then
+if [ "$nic1" != 0 ]; then
 
 	# verify that nodes array is circularly sorted
 	# this is required for the definition of this topology
@@ -77,6 +80,12 @@ elif [ "$nic1" != 0 ]; then
 
 	ip route add 10.10."$network"."${ips[0]}" dev "$nic0"
 	ip route add 10.10."$network"."${ips[1]}" dev "$nic1"
+
+	# to achieve high speeds, increase mtu
+	[ "$(hostname | grep -cE "meld|tinyman|yieldly|gard|goracle|zone")" -eq 1 ] && {
+		ip link set dev "$nic0" mtu 9700
+		ip link set dev "$nic1" mtu 9700
+	}
 
 # here the testhosts are connected via switch
 else
