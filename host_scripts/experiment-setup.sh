c55bb94cdf3d89ac6449fa3cd5b214060b2070be
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2023-02-16 17:07:57 +0100

    test 25g link

--- host_scripts/experiment-setup.sh
+++ host_scripts/experiment-setup.sh
@@ -50,39 +50,39 @@ ips=()
 ### three nodes indirect connection topology setup
 ### node 2 --- node 1 --- node 3
 ### for 25G+ speeds set MTU
-if [ "$(hostname | grep -cE "gard|goracle|zone")" -eq 1 ]; then
-
-	# install ddp drivers
-	installDriver
-
-	# store other participants ips
-	for i in $(seq 2 $((groupsize+1))); do
-		[ "$ipaddr" -ne "$i" ] && ips+=( "$i" )
-	done
-
-	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic0"
-	ip link set dev "$nic0" mtu 9700
-	ip link set dev "$nic0" up
-
-	if [ "$ipaddr" -eq 2 ]; then
-		# activate forwarding for the center node
-		sysctl -w net.ipv4.ip_forward=1
-		ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic1"
-		ip link set dev "$nic1" mtu 9700
-		ip link set dev "$nic1" up
-		# route via correct NICs
-		ip route add 10.10."$network".3 dev "$nic0"
-		ip route add 10.10."$network".4 dev "$nic1"
-	# this is probably not required since the routes should be set automatically
-	elif [ "$ipaddr" -eq 3 ]; then
-		ip route add 10.10."$network".4 via 10.10."$network".2
-	else
-		ip route add 10.10."$network".3 via 10.10."$network".2
-	fi
+###if [ "$(hostname | grep -cE "gard|goracle|zone")" -eq 1 ]; then
+###
+###	# install ddp drivers
+###	installDriver
+###
+###	# store other participants ips
+###	for i in $(seq 2 $((groupsize+1))); do
+###		[ "$ipaddr" -ne "$i" ] && ips+=( "$i" )
+###	done
+###
+###	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic0"
+###	ip link set dev "$nic0" mtu 9700
+###	ip link set dev "$nic0" up
+###
+###	if [ "$ipaddr" -eq 2 ]; then
+###		# activate forwarding for the center node
+###		sysctl -w net.ipv4.ip_forward=1
+###		ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic1"
+###		ip link set dev "$nic1" mtu 9700
+###		ip link set dev "$nic1" up
+###		# route via correct NICs
+###		ip route add 10.10."$network".3 dev "$nic0"
+###		ip route add 10.10."$network".4 dev "$nic1"
+###	# this is probably not required since the routes should be set automatically
+###	elif [ "$ipaddr" -eq 3 ]; then
+###		ip route add 10.10."$network".4 via 10.10."$network".2
+###	else
+###		ip route add 10.10."$network".3 via 10.10."$network".2
+###	fi
+#### three nodes direct connection topology if true
+###elif [ "$nic1" != 0 ]; then
 # three nodes direct connection topology if true
-elif [ "$nic1" != 0 ]; then
-# three nodes direct connection topology if true
-####if [ "$nic1" != 0 ]; then
+if [ "$nic1" != 0 ]; then
 
 	# to achieve high speeds, install ddp drivers
 	[ "$(hostname | grep -cE "meld|tinyman|yieldly|gard|goracle|zone")" -eq 1 ] && \
@@ -150,7 +150,7 @@ source "$REPO2_DIR"/tools/speedtest.sh
 
 	for serverip in $(seq 2 $((groupsize+1))); do
 		for clientip in $(seq 2 $((groupsize+1))); do
-			pos_sync
+			#pos_sync
 			# skip the server
 			[ "$serverip" -eq "$clientip" ] && continue
 			# skip other clients for now
