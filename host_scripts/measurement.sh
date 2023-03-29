cfe025203ce0cb88a36e6bce24e1b1b6623c8186
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2023-03-29 13:55:14 +0200

    fix stuck loops

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -61,7 +61,7 @@ fi
     # set config and compile experiment
     if [ "$splitroles" -eq 0 ]; then
         /bin/time -f "$timerf" ./Scripts/config.sh -p "$player" -n "$size" -d "$datatype" \
-            -s "$protocol" -e "$preprocess" -c "$packbool" -o "$optshare" -h "$ssl" -b 6000
+            -s "$protocol" -e "$preprocess" -c "$packbool" -o "$optshare" -h "$ssl" -b 20000
     else
         # with splitroles active, "-p 3" would through error. Omit -p as unneeded
         /bin/time -f "$timerf" ./Scripts/config.sh -n "$size" -d "$datatype" \
