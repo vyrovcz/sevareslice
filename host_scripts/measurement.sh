ee5fc3a002da46cac2550806291f158f91dde66c
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2023-03-31 21:33:20 +0200

    fix variable name

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -24,7 +24,7 @@ packbool=$(pos_get_variable packbool --from-loop)
 optshare=$(pos_get_variable optshare --from-loop)
 ssl=$(pos_get_variable ssl --from-loop)
 threads=$(pos_get_variable threads --from-loop)
-function=$(pos_get_variable function --from-loop)
+fun=$(pos_get_variable function --from-loop)
 
 timerf="%M (Maximum resident set size in kbytes)\n\
 %e (Elapsed wall clock time in seconds)\n\
@@ -62,12 +62,12 @@ fi
     if [ "$splitroles" -eq 0 ]; then
         /bin/time -f "$timerf" ./Scripts/config.sh -p "$player" -n "$size" -d "$datatype" \
             -s "$protocol" -e "$preprocess" -c "$packbool" -o "$optshare" -h "$ssl" -b 25000 \
-            -j "$threads" -f "$function"
+            -j "$threads" -f "$fun"
     else
         # with splitroles active, "-p 3" would through error. Omit -p as unneeded
         /bin/time -f "$timerf" ./Scripts/config.sh -n "$size" -d "$datatype" \
             -s "$protocol" -e "$preprocess" -c "$packbool" -o "$optshare" -h "$ssl" -b 25000 \
-            -j "$threads" -f "$function"
+            -j "$threads" -f "$fun"
     fi
     
     [ "$splitroles" -eq 1 ] && ./Scripts/split-roles-3-compile.sh -p "$player" -a "$ipA" -b "$ipB"
